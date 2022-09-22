41847da7b97b690d3452d791c862f6cd52c534ec
man2020duh <107978599+man2020duh@users.noreply.github.com>
2022-09-22 16:54:59 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+const st=ScrollReveal({duration:2000,reset:true});
+/*
+st.reveal('h1',{
+    origin:'right',
+    distance:'150px',
+scale:0.5,
+delay:500
+});*/
+st.reveal('.col-right',{
+    origin:'right',    
+    distance:'100%',
+delay:500
+});
+st.reveal('.col-left',{
+    origin:'top',   
+    distance:'100%',   
+    delay:500
+});
+/*
+st.reveal('.item',{
+    delay:1000
+},50);*/
